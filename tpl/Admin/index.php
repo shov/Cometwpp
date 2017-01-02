97e1d5517b0b7b220383f39cd48d53d4dad4369b
Shov <ls.shov@gmail.com>
2017-01-02 21:48:58 +0300

    base Admin tpl

---
+++ tpl/Admin/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Cometwpp package.
+ *
+ * (c) Alexandr Shevchenko [comet.by] alexandr@comet.by
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cometwpp;
+
+use Cometwpp\Core\Core;
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+?>
+    <h1><?= Core::getInstance()->getName() ?></h1>
+    <?php
+echo $part;
