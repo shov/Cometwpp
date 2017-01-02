8ee1c9c6687b8f188f903e593ced34696bdaf15f
Shov <ls.shov@gmail.com>
2017-01-02 20:47:53 +0300

    install script

--- install.sh
+++ install.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
+
+GL='\033[1;32m'
+NC='\033[0m' # No Color
+
 if [ ! -z "$1" ]; then
     if [ "$1" == '--update' ]; then
-        echo "Don't know how to update yet :(";
+        echo -e "${GL}Don't know how to update yet :(";
         exit
     fi
 
     name=$1
     rx="^([a-zA-Z]{3,})$"
     if ! [[ "$name" =~ $rx ]] ; then
-        echo "Bad name, use letters in the range [A-Za-z]!"
+        echo -e "${GL}Bad name, use letters in the range [A-Za-z]!"
         exit
     fi
 
     rm -Rf ./.git/ &&
     rm -f ./.gitignore &&
-    echo "Git files has been removed"
+    echo -e "${GL}Git files has been removed"
 
-    cat ./config.example.php | sed --quiet "s/cometwpp_plugin_fw_prefix/$name-prefix/g" | sed --quiet "s/cometwpp_plugin_fw/$name/g" > ./config.php &&
+    cat ./config.example.php | sed "s/cometwpp_plugin_fw_prefix/$name-prefix/g" | sed "s/cometwpp_plugin_fw/$name/g" > ./config.php &&
     rm -f ./config.example.php &&
-    echo "* Config done"
+    echo -e "${GL}* Config done"
 
-    find ./ -type f -name "*.php" -print0 | xargs -0 sed -i --quiet "s/Cometwpp/$name/g"
-    echo "* Namespace done"
+    find ./ -type f -name "*.php" -print0 | xargs -0 sed -i "s/Cometwpp/$name/g"
+    echo -e "${GL}* Namespace done"
 
-    cat ./pl_plugin_name.php | sed --quiet "s/PluginName/$name/g" > ./"$name".php  &&
+    cat ./pl_plugin_name.php | sed "s/PluginName/$name/g" > ./"$name".php  &&
     rm -f ./pl_plugin_name.php &&
-    echo "* Plugin Name done"
+    echo -e "${GL}* Plugin Name done"
 
     cd .. &&
     mv -T ./Cometwpp ./"$name" &&
     cd `pwd -P` &&
-    echo "* Plugin directory has been renamed" &&
-    echo "* Install complete"
-else echo "Take the plugin name as argument in cli"
+    echo -e "${GL}* Plugin directory has been renamed" &&
+    echo -e "${GL}* Install complete"
+else echo -e "${GL}Take the plugin name as argument in cli"
 fi
